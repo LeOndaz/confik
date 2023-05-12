d0166c39e3289ec8000f25ea1767e8e71cd71a99
Ahmed Ibrahim <leondaz@macbook.local>
2023-05-12 09:34:23 +0300

    add docstring for get

--- confik/__init__.py
+++ confik/__init__.py
@@ -13,4 +13,14 @@ def get(
     choices=tuple(),
     raise_exception=False,
 ):
+    """An interface to confik.get(...)
+
+    :param key: The key name to get from environment
+    :param default: The value to return if the key was not found
+    :param cast: A function to call on the value after it's found
+    :param default_factory: A function to call with the key to return a value if the value was not found
+    :param choices: A choices list to ensure that the value is in it
+    :param raise_exception: Raise exceptions if the value was not found
+    :return: str
+    """
     return confik.get(key, default, cast, default_factory, choices, raise_exception)
